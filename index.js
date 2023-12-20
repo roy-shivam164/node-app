8d6a6f449701da5bcd13eff3f5e1a63381e5bc27
roy-shivam164 <shivam.rai@cginfinity.com>
2023-12-20 16:18:53 +0530

    removed the normalize port

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// const createError = require('http-errors');
+const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -20,23 +20,23 @@ app.get('/', (req, res) =>{
 })
 
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
 // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 // Get port from environment and store in Express.
-const port = normalizePort(process.env.PORT || '3000');
+const port = 3001;
 app.set('port', port);
 
 // Create HTTP server.
@@ -44,5 +44,3 @@ const server = app.listen(port, function() {
   console.log('Express server listening on port ' + server.address().port);
 });
 
-// Normalize a port into a number, string, or false.
-//  
