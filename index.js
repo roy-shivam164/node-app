43afff83461ef4bd82eb3e6c783777687f2722d8
roy-shivam164 <shivam.rai@cginfinity.com>
2023-12-20 14:58:22 +0530

    comments removed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// const createError = require('http-errors');
+const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -20,20 +20,20 @@ app.get('/', (req, res) =>{
 })
 
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
 // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 // Get port from environment and store in Express.
 const port =  3001;
@@ -43,3 +43,4 @@ app.set('port', port);
 const server = app.listen(port, function() {
   console.log('Express server listening on port ' + server.address().port);
 });
+
