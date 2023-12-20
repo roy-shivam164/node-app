ebb4b97fef810ac0dcb326e5f8d6f57481a912a0
roy-shivam164 <shivam.rai@cginfinity.com>
2023-12-20 12:23:08 +0530

    updated port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Set up a route to serve the HTML file
 app.get('/', (req, res) => {
